c068df5cb0efa982f71489779ad080b889633ddc
berzezek <wknduz@gmail.com>
2024-04-16 12:19:05 +0500

    Create places app

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('afisha.urls')),
+    path('', include('places.urls')),
 ]
--- backend/places/urls.py
+++ backend/places/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from afisha.views import index
+from places.views import index
 
 urlpatterns = [
     path("", index),

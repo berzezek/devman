5b00b5bb2737b84976706cc048266357765f0897
berzezek <wknduz@gmail.com>
2024-04-16 12:53:19 +0500

    Add model for images

--- backend/places/admin.py
+++ backend/places/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from places.models import Place
+from places.models import Place, PlaceImage
 
 
 admin.site.register(Place)
+admin.site.register(PlaceImage)
--- backend/places/models.py
+++ backend/places/models.py
@@ -7,6 +7,15 @@ class Place(models.Model):
     description_long = models.TextField()
     latitude = models.FloatField()
     longitude = models.FloatField()
-    
+
     def __str__(self):
         return self.title
+
+
+class PlaceImage(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="images/")
+    image_number = models.PositiveSmallIntegerField(unique=True)
+
+    def __str__(self):
+        return f"{self.image_number} {self.place.title}"

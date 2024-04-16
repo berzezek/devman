b9e431f4bb0f6e17046014100769aa720b8cef95
berzezek <wknduz@gmail.com>
2024-04-16 19:10:08 +0500

    add get preview in admin site

--- backend/places/admin.py
+++ backend/places/admin.py
@@ -1,31 +1,31 @@
 from django.contrib import admin
 from .models import Place, PlaceImage
 from django.utils.safestring import mark_safe
-from django.utils.html import format_html
 
 
 class PlaceImageInline(admin.TabularInline):
     model = PlaceImage
-    extra = 1  # Количество пустых форм для добавления новых изображений
-    fields = ("image", "image_number")  # Укажите поля, которые вы хотите отображать
-    readonly_fields = (
+    extra = 1
+    fields = (
+        "image",
         "current_image",
-    )  # Если вы хотите показать текущее изображение, добавьте это в readonly_fields
+        "image_number",
+    )
+    readonly_fields = ("current_image",)
 
     def current_image(self, instance):
-        # Возвращает HTML для отображения текущего изображения; требуется, если вы добавили его в readonly_fields
         if instance.image:
             return mark_safe(
-                '<img src="{}" width="150" height="150" />'.format(instance.image.url)
+                '<img src="{}" style="max-height: 200px" />'.format(instance.image.url)
             )
         return "-"
 
-    current_image.short_description = "Currently"  # Название колонки
+    current_image.short_description = "Get preview"
 
 
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ("title", "latitude", "longitude")  # Какие поля отображать в списке
-    inlines = [PlaceImageInline]  # Добавляем inline для PlaceImage
+    list_display = ("title", "latitude", "longitude")
+    inlines = [PlaceImageInline]
 
 
 admin.site.register(Place, PlaceAdmin)

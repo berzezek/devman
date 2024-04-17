fd3d3cdb8cba5ca0aea04ba13efc61d111b9c1ca
berzezek <wknduz@gmail.com>
2024-04-17 13:51:58 +0500

    add adminsortable2 for models Place, PlaceImage

--- backend/places/admin.py
+++ backend/places/admin.py
@@ -1,31 +1,27 @@
 from django.contrib import admin
 from .models import Place, PlaceImage
 from django.utils.safestring import mark_safe
+from adminsortable2.admin import SortableTabularInline, SortableAdminMixin
 
 
-class PlaceImageInline(admin.TabularInline):
+class PlaceImageTabularInline(SortableTabularInline):
     model = PlaceImage
     extra = 1
     fields = (
         "image",
         "current_image",
-        "image_number",
     )
     readonly_fields = ("current_image",)
-
     def current_image(self, instance):
         if instance.image:
             return mark_safe(
                 '<img src="{}" style="max-height: 200px" />'.format(instance.image.url)
             )
         return "-"
-
     current_image.short_description = "Get preview"
 
 
-class PlaceAdmin(admin.ModelAdmin):
-    list_display = ("title", "latitude", "longitude")
-    inlines = [PlaceImageInline]
-
-
-admin.site.register(Place, PlaceAdmin)
+@admin.register(Place)
+class PlaceAdmin(SortableAdminMixin, admin.ModelAdmin):
+    fields = ("title", "description_short", "description_long", "latitude", "longitude")
+    inlines = [PlaceImageTabularInline]
--- backend/places/models.py
+++ backend/places/models.py
@@ -14,18 +14,22 @@ class Place(models.Model):
         return self.title
 
     class Meta:
+        ordering = ["pk"]
         verbose_name = "Место"
         verbose_name_plural = "Места"
 
 
 class PlaceImage(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
-    image = models.ImageField(verbose_name="Картика", upload_to="images/")
-    image_number = models.PositiveSmallIntegerField(verbose_name="Позиция", unique=True)
+    image = models.ImageField(verbose_name="Картинка", upload_to="images/")
+    image_number = models.PositiveSmallIntegerField(
+        verbose_name="Позиция", blank=True, null=True, default=0
+    )
 
     def __str__(self):
         return f"{self.image_number} {self.place.title}"
 
     class Meta:
+        ordering = ["image_number"]
         verbose_name = "Фотография"
         verbose_name_plural = "Фотографии"
